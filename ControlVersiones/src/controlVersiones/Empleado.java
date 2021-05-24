d102af4726855adcbac946fcf4253cd0992f85c7
1dami <1dami@hz319578.TartangaLH.eus>
2021-05-24 09:18:20 +0200

    Este commit debería generar un conflicto al intentar subir una version desactualizada en la master

--- ControlVersiones/src/controlVersiones/Empleado.java
+++ ControlVersiones/src/controlVersiones/Empleado.java
@@ -1,11 +1,27 @@
 package controlVersiones;
 
 public class Empleado {
+	/**
+	 * Este atributo recoge el nombre del empleado guardado en una variable de tipo string
+	 */
 	private String nombre;
+	/**
+	 * Este atributo recoge el apellido del empleado guardado en una variable de tipo string
+	 */
 	private String apellidos;
+	/**
+	 * Este atributo recoge el salario que va a cobrar el empleado guardado en una variable de tipo double
+	 */
 	private double salario; 
 
 
+	/**
+	 * CONSTRUCTOR
+	 * 
+	 * @param nombre recibe el nombre de fuera para crear un empleado con dicho nombre
+	 * @param apellido recibe el apellido de fuera para crear un empleado con dicho apellido
+	 * @param salario recibe el salario de fuera para crear un empleado con dicho salario
+	 */
 public Empleado(String nombre, String apellido, double salario) {
 	this.nombre = nombre;
 	this.apellidos = apellido;
@@ -33,6 +49,12 @@ public void setSalario(double salario) {
 	this.salario = salario;
 }
 
+
+/**
+ * 
+ * @param subida recibe un double que será lo que se le sumará al salario y suma al salario que ese empleado tiene, el salario recibido
+ * EX: Salario = 100 / Subida = 20 entonces: El nuevo salario será 120
+ */
 public void subirsalario (double subida) {
 	salario=salario+subida;
 }
